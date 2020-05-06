d265860173952024b59d42b651e736239cb35ac8
mac <max2digitway@outlook.jp>
2020-05-06 11:15:58 +0900

    dot.emacsの初期ファイルを登録

---
+++ emacs/my_setting/init.el
@@ -0,0 +1,4 @@
+(global-set-key "\M-!" 'delete-other-windows)
+(global-set-key "\M-\"" 'split-window-vertically)
+(global-set-key "\M-#" 'split-window-horizontally)
+(global-set-key "\M-o" 'other-window)
